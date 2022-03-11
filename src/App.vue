0328b870bda9328c573e3ba6d765b08d92b53042
Meeno14 <helmi.abdillah01@gmail.com>
2022-03-11 13:17:58 +0700

    Biografi_vue

--- src/App.vue
+++ src/App.vue
@@ -5,20 +5,17 @@
       <br />
       <agenda-input />
       <br />
-      <agenda-show />
     </div>
   </div>
 </template>
 
 <script>
 import AgendaInput from "./components/AgendaInput.vue";
-import AgendaShow from "./components/AgendaShow.vue";
 
 export default {
   name: "App",
   components: {
     AgendaInput,
-    AgendaShow,
   },
 };
 </script>
--- src/components/AgendaInput.vue
+++ src/components/AgendaInput.vue
@@ -1,132 +1,121 @@
 <style>
-    /* body {
-        background-color: black;
-        font-family: serif;
-    } */
-    /* .app{
-        margin: 10px;
-    padding: 80px;
-    border-radius: 1px;
-    position: relative;
-    left: 300px;
-    background-color: rgb(94, 88, 88);
-    size: 100%;
-    width: 270px;
-    } */
-    .op {
-        background-color: rgb(230, 228, 228);
-        border-radius: 1px;
-        position: relative;
-        width: 500px;
-        border: 2px;
-        padding: 20px;
-    }
- 
-    .pp {
-        background-color: whitesmoke;
-        border-radius: 1px;
-        position: relative;
-        width: 900px;
-        border: 2px;
-        padding: 20px;
-        
-    }
- 
-    img {
-        width: 250px;
-    }
-    p,
-    ul,
-    li {
-        font-size: 15px;
-    }
+body {
+  background-color: rgb(53, 230, 230);
+  border-radius: 1px;
+  position: relative;
+  border: 2px;
+  padding: 20px;
+  font-family: Arial, Helvetica, sans-serif;
+}
+.p2 {
+  margin: 10px;
+  padding: 80px;
+  border-radius: 1px;
+  position: relative;
+  left: 200px;
+  background-color: aqua;
+  size: 100%;
+  width: 600px;
+  color: black;
+}
+.p3 {
+  text-align: center;
+}
+.lane1 {
+  color: black;
+  text-align: center;
+}
+.h {
+  color: black;
+}
+img {
+  width: 440px;
+  height: 350px;
+}
+ul {
+  list-style-type: none;
+}
 </style>
  <template>
- <div>
-     
- <body >
-<div style="display: flex;" class="app">
-    <div class="op">
-    <center>
-        <h1>Arya<span style="color: tomato;">Dika</span></h1>
-    </center>
-        <img src="https://images.bisnis-cdn.com/thumb/posts/2019/01/29/883405/bisnis.jpg?w=600&h=400"><br><br>
- 
-        <h3>Contact<hr></h3>
-        <p><b>Address:</b><br>
-        177 Great Portland Street, London W5W 6PQ</p>
-        <p><b>Phone:</b><br>
-        +62 821 9530 7113</p>
-        <b>Email:</b><br>
-        <u>fajar.a@gmail.com</u><br><br>
-        <b>Linkedln:</b><br>
-        <u>linkedin.com/fajar.ariyanto</u><br><br>
- 
-        <h3>Languages<hr></h3>
-       
-           Spanish - C2 <br>
-           Chinese - A1 <br>
-           German - A2 <br>
-         
-        <h3>Hobbies
-            <hr>
-        </h3>
-        <ul>
-            <li>Writing</li>
-            <li>Sketching</li>
-            <li>Photography</li>
+  <body>
+    <div class="p2">
+      <h2 class="lane1"><strong>Biografi</strong></h2>
+      <hr class="h" />
+      <img
+        class="lane2"
+        src="https://awsimages.detik.net.id/community/media/visual/2020/10/16/kimetsu-no-yaiba-movie-demon-slayer-1_169.jpeg?w=700&q=90"
+      />
+      <hr class="h" />
+      <div class="p3">
+        <center>
+          <h3><strong>Contact :</strong></h3>
+          <hr />
+          <p>
+            <b>Name : </b><br />
+            Arya Dika Handi Saputra
+          </p>
+          <p>
+            <b>Address:</b><br />
+            Jl.Prigi Rt 07 Rw 06 Mranggen, Demak
+          </p>
+          <p>
+            <b>Phone:</b><br />
+            +62 831 0213 2052
+          </p>
+          <b>Email:</b><br />
+          <u>aryadika@smkbinusademak.sch.id</u><br /><br />
+          <b>Linkedln:</b><br />
+          <u>linkedin.com/aryadika</u><br /><br />
+
+          <h3>
+            <strong>Languages :</strong>
+            <hr class="h" />
+          </h3>
+
+          English - C2 <br />
+          Japan - A1 <br />
+          Indonesian - A2 <br /><br />
+
+          <h3>
+            <strong>Hobbies :</strong>
+            <hr class="h" />
+          </h3>
+          <ul>
+            <li>Adventure</li>
+            <li>Programer</li>
+            <li>Listen to Sing</li>
             <li>Design</li>
-        </ul>
+          </ul>
+
+          <h3><strong>Skill Highlights :</strong></h3>
+          <br />
+
+          <hr class="h" />
+          <ul>
+            <span>
+              <li>Project management</li>
+              <li>Creative design</li>
+              <li>Frontend Devoloper</li>
+            </span>
+          </ul>
+
+          <hr class="h" />
+          <p>
+            PHP framework (certificate):
+            <b>Zend, Codeigniter, Symfony.</b> Programming languages:
+          </p>
+          <div>HTML5, Java, JavaScript, CSS</div>
+        </center>
+      </div>
     </div>
-    <div class="pp">
-        <h3>Summary</h3>
-            <hr>
-            <p>Senior Web Developer specializing in front end development. Experienced whit all stages of the
-                development
-                cycle for dynamic web project. Well-versed in numerous programming languages including HTML5, PHP OOP,
-                JavaScript, CSS, MySQL. Strong backgruond in project management and customer relations</p>
- 
-            <h3>Skill Highlights</h3>
-            <hr>
-            <ul> <span>
-                <li>Project management</li> <li style="padding-right: 10px;">Creative design</li>
-                <li>Strong decision maker</li><li>Innovative</li>
-                <li>Complex problem solver</li><li>Service-focused</li></span>
-            </ul>
- 
-            <h3>Experience</h3>
-            <hr>
-            <b>Web Develover</b> - 09/2015 to 05/2019 <br>
-            <b>Luna Web Design</b>, New York
-            <ul>
-                <li>Cooperate With designers to create clean interfaces and simple, intuitive interactions and
-                    experience.
-                </li>
-                <li>Develop project concepts and maintain optimal workflow.</li>
-                <li>Work whit senior developer to manage large, complex design project for corporate clients.</li>
-                <li>Complete detailed programming and development tasks for front end public and internal websites as
-                    well
-                    as challenging back and server code.</li>
-                <li>carry out quality assurance tests to discover errors and optimize usability.</li>
-            </ul>
-            <h3>Education</h3>
-            <hr>
-            Bachelor of science: <b>Computer information System - 2014 Columbia University</b>, NY
-            <h3>Certifications</h3>
-            <hr>
-            <p>PHP framework (certificate): <b>Zend, Codeigniter, Symfony.</b> Programming languages: <div> HTML5, PHP
-                    OOP,
-                    JavaScript, CSS
-        </div>
-        </body>
-        </div>
-        </template>
+  </body>
+</template>
 <script>
 export default {
-    nama: "AgendaShow",
-    computed: {
-        agenda() {
-            return this.$store.state.listAgenda
-        },
+  nama: "AgendaShow",
+  computed: {
+    agenda() {
+      return this.$store.state.listAgenda;
     },
+  },
 };
